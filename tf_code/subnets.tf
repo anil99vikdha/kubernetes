3a35419ae229f80acdbce357c20c3ee8dbeb6f49
anil99vikdha <anilkumar.mah999@gmail.com>
2026-01-17 10:24:11 +0530

    vpc + eks terraform code

---
+++ tf_code/subnets.tf
@@ -0,0 +1,49 @@
+resource "aws_subnet" "private_az1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.3.0/24"
+  availability_zone = local.zone1
+
+  tags = {
+    "Name"                                                 = "${local.env}-private-${local.zone1}"
+    "kubernetes.io/role/internal-elb"                      = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "private_az2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.4.0/24"
+  availability_zone = local.zone2
+
+  tags = {
+    "Name"                                                 = "${local.env}-private-${local.zone2}"
+    "kubernetes.io/role/internal-elb"                      = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_az1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.1.0/24"
+  availability_zone       = local.zone1
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                                 = "${local.env}-public-${local.zone1}"
+    "kubernetes.io/role/elb"                               = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "public_az2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.2.0/24"
+  availability_zone       = local.zone2
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                                                 = "${local.env}-public-${local.zone2}"
+    "kubernetes.io/role/elb"                               = "1"
+    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
+  }
+}
